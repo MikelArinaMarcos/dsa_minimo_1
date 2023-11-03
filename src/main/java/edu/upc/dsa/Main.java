d22b3a1534458d15cc2ca9e41a923539feb5924d
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-11-03 09:25:27 +0100

    Clase Main modificada y sin errores

--- src/main/java/edu/upc/dsa/Main.java
+++ src/main/java/edu/upc/dsa/Main.java
@@ -29,8 +29,8 @@ public class Main {
 
         beanConfig.setHost("localhost:8080");
         beanConfig.setBasePath("/dsaApp");
-        beanConfig.setContact("support@example.com");
-        beanConfig.setDescription("REST API for Minimo 1");
+        beanConfig.setContact("mikelarinamarcos@gmail.com");
+        beanConfig.setDescription("REST API del examen del Minimo 1 de DSA");
         beanConfig.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
         beanConfig.setResourcePackage("edu.upc.dsa.Service");
         beanConfig.setTermsOfServiceUrl("http://www.example.com/resources/eula");
@@ -56,4 +56,3 @@ public class Main {
         server.stop();
     }
 }
-
